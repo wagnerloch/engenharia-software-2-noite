7343a212a6081fafc59f54926facf9810422c3bc
Wagner Loch <wagner_loch@hotmail.com>
2026-08-18 20:42:45 -0300

    Add factorial function

--- Main.js
+++ Main.js
@@ -23,3 +23,12 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(10));
+
+function factorial(n) {
+    if (n === 0) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
